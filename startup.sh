5cd3ab3a3932f1fa6cd8f659d49838e84def2df3
Ansil H <ansilh@gmail.com>
2019-07-15 02:45:09 +0530

    container should wait for /var/www/mediawiki

--- startup.sh
+++ startup.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+until ls -ld /var/www/mediawiki; do echo "Waiting for volume";sleep 5 ;done
 chown -R nginx:nginx /var/www
-php-fpm7 -F &
+php-fpm7 -F >/dev/stdout 2>&1 &
 nginx -g 'daemon off;'
